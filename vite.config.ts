e3283a2b4be05fb0742eef627c3606d176dab825
Michele Sciabarra <michele@sciabarra.com>
2025-04-14 20:07:06 +0000

    configuring ops dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 5173,
+		proxy: {
+			"/api/my": {
+				target: process.env.OPSDEV_HOST,
+				changeOrigin: true
+			}
+		}
+	}
 });
